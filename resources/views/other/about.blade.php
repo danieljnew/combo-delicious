36f9e07a281f6509685c13e741d048499e3f3687
danieljnew <danieljnew@gmail.com>
2017-05-28 14:33:27 -0500

    testing this stuff out

---
+++ resources/views/other/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('content')
+  <div class="row">
+    <div class="col-md-12">
+      <p class="quote">About Me</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <p>Typing a bunch of stuff here I hope all of this works
+      But I am not super confident. Nevertheless we shall see.</p>
+    </div>
+  </div>
+@endsection
