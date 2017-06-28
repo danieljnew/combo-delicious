29bf7e95bd3674fa069b44fc2ddfc56c20377a09
danieljnew <danieljnew@gmail.com>
2017-06-27 22:10:59 -0500

    testing atom integration

---
+++ app/post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+class Post 
+{
+	public function getPosts($session)
+	{
+		if (!$session->has('posts')){
+			$this->createDummyData($session);
+		}
+		return $session->get('posts');
+	}
+
+	private function createDummyData()
+	{
+		$posts = [
+		[
+			'title' => 'Learning Laravel',
+			'content' => 'This blog post will get you right on track with Laravel'
+		],
+		[
+			'title' => 'Something else',
+			'content' => 'Some other content'
+		]
+
+		];
+		$session->put('posts', $posts);
+	}
+}
