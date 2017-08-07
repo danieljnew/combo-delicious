e15551102d3955089b73a4a498c4c2f731b32f01
danieljnew <danieljnew@gmail.com>
2017-08-07 14:23:38 -0500

    Added manytomany relationship table info and queried tables to display

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use App\Like;
+use App\Tag;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -28,7 +29,7 @@ class PostController extends Controller
     {
       $post = Post::where('id', $id)->with('likes')->first();
       return view('blog.post', ['post' => $post]);
-    },
+    }
     public function getLikePost($id)
     {
       $post = Post::where('id', $id)->first();
@@ -38,12 +39,14 @@ class PostController extends Controller
     }
     public function getAdminCreate()
     {
-      return view('admin.create');
+      $tags = Tag::all();
+      return view('admin.create', ['tags' => $tags]);
     }
     public function getAdminEdit($id)
     {
       $post = Post::find($id);
-      return view('admin.edit', ['post' => $post, 'postId' => $id]);
+      $tags = Tag::all();
+      return view('admin.edit', ['post' => $post, 'postId' => $id, 'tags' => $tags]);
     }
     public function postAdminCreate(Request $request)
     {
@@ -57,6 +60,7 @@ class PostController extends Controller
           'content' => $request->input('content')
       ]);
       $post->save();
+      $post->tags()->attach($request->input('tags') === null ? [] : $request->input('tags'));
       return redirect()->route('admin.index')->with('info', 'Post created, Title is:'
       . $request->input('title'));
     }
@@ -70,6 +74,8 @@ class PostController extends Controller
       $post->title = $request->input('title');
       $post->content = $request->input('content');
       $post->save();
+      // $post->tags()->attach($request->input('tags') === null ? [] : $request->input('tags'));
+      $post->tags()->sync($request->input('tags') === null ? [] : $request->input('tags'));
       return redirect()->route('admin.index')->with('info', 'Post edited, new title
       is: ' . $request->input('title'));
     }
@@ -77,6 +83,7 @@ class PostController extends Controller
     {
       $post = Post::find($id);
       $post->likes()->delete();
+      $post->tags()->detach();
       $post->delete();
       return redirect()->route('admin.index')->with('info', 'Post deleted!');
     }
